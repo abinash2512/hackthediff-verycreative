4601ac16162bef471b37b642d5989472b6238541
Abinash <RameshA@cardiff.ac.uk>
2023-02-23 13:00:44 +0000

    back to windows

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
